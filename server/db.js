03ef8ab8f755714d706ca5aa47b8f1199d7eaa7e
fagtic <68462729+fagtic@users.noreply.github.com>
2026-05-26 22:44:38 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'admin', 
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } 
 });
 
 module.exports = pool;
